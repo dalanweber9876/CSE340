60442583ee1fa90df12228620dbe0b8e4193b0c8
Dalan Weber <dalanweber@Dalans-MacBook-Air.local>
2025-05-07 13:16:49 -0600

    Fixed database issues

--- database/rebuild_db copy.sql
+++ database/rebuild_db copy.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO dalanweber;
+    OWNER TO weber35391;
 
 
 -- Table structure for table 'classification'
